9085d8dfe016a53439310fa7b7538dc259f6395f
VennelaReddy408 <podduturivennela@gmail.com>
2024-10-28 11:12:13 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,45 @@
+// // examples pd switch
+// switch("wednesday"){
+//     case "monday":
+//         console.log("monday is the first of week")
+//         break;
+//         case "tuesday":
+//             console.log("tuesday is special for hauman")
+//             break;
+//             case "wednesday":
+//                 console.log("wednesday is my fav day")
+//                 break;
+//                 case "thursday":
+//                     console.log("thursday is special for us ")
+//                     break;
+//                     case "friday":
+//                         console.log("friday os special for laxmi devi")
+//                         break;
+//                         case "saturday":
+//                             console.log("saturday is weekend")
+//                             break;
+//                             case "sunday":
+//                                 console.log("sunday os holiday")
+//                                 break;
+//                                 default:
+//                                     console.log("please select any day")
+
+
+// }
+
+
+// examples of for of
+var b="vamsi"
+for(var char of b){
+   
+
+console.log(char)
+}
+
+
+var a=[1,2,3,4,5]
+var totalSum=0
+for(var sum of a){
+    totalSum=totalSum+sum
+}
+console.log(totalSum)
